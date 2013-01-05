66c7d0c2277637fdb3b0c48f40c451e6958481b5
Harrison Brown (iMac) <harrisonbro@gmail.com>
2013-01-05 19:13:06 +0000

    Got working proof-of-concept for reading pattern files into template

--- site/templates/default.php
+++ site/templates/default.php
@@ -7,9 +7,19 @@
   <article>
     <h1><?php echo html($page->title()) ?></h1>
     <?php echo kirbytext($page->description()) ?>
-    <?php echo kirbytext($page->html()) ?>
-    <?php echo kirbytext($page->php()) ?>
-    <?php echo kirbytext($page->css()) ?>
+    <?php 
+      function get_pattern_file( $filename = null, $page = null ) {
+        if( !$filename || !$page ) { return false; }
+        $file = $page->files()->find( $filename );
+        if( !$file ) { return false; } // If no file found
+        else { return file_get_contents( $file->root() ); }
+      }
+      
+      if( get_pattern_file('php.php', $page) ) { echo get_pattern_file('php.php', $page); }
+      if( get_pattern_file('html.html', $page) ) { echo get_pattern_file('html.html', $page); }
+      if( get_pattern_file('css.css', $page) ) { echo get_pattern_file('css.css', $page); }
+
+    ?>
   </article>
 
 </section>

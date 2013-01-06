426a772dab5ab33696baeb70f41ba1954dc8c211
Harrison Brown (iMac) <harrisonbro@gmail.com>
2013-01-06 11:53:14 +0000

    Refactored checking and getting of pattern files into functions

--- site/templates/default.php
+++ site/templates/default.php
@@ -8,16 +8,21 @@
     <h1><?php echo html($page->title()) ?></h1>
     <?php echo kirbytext($page->description()) ?>
     <?php 
-      function get_pattern_file( $filename = null, $page = null ) {
-        if( !$filename || !$page ) { return false; }
-        $file = $page->files()->find( $filename );
+      function has_pattern_file( $filename = null, $page = null ) {
+        if( !$filename || !$page ) { return false; } // Check for required inputs
         if( !$file ) { return false; } // If no file found
-        else { return file_get_contents( $file->root() ); }
-      }
+      } // END has_pattern_file()
       
-      if( get_pattern_file('php.php', $page) ) { echo get_pattern_file('php.php', $page); }
-      if( get_pattern_file('html.html', $page) ) { echo get_pattern_file('html.html', $page); }
-      if( get_pattern_file('css.css', $page) ) { echo get_pattern_file('css.css', $page); }
+      function get_pattern( $filename = null, $page = null ) {
+        if( !$filename || !$page ) { return false; } // Check for required inputs
+        if( !has_pattern_file( $filename, $page ) ) { return false; } // Check input file exists
+        $file = $page->files()->find( $filename );
+        else { return file_get_contents( $file->root() ); }
+      } // END get_pattern_file()
+      
+      if( has_pattern_file('php.php', $page) ) { echo get_pattern('php.php', $page); }
+      if( has_pattern_file('html.html', $page) ) { echo get_pattern('html.html', $page); }
+      if( has_pattern_file('css.css', $page) ) { echo get_pattern('css.css', $page); }
 
     ?>
   </article>

dd5dea170fbe277c9237b55fde132693ea63dc39
Harrison Brown (iMac) <harrisonbro@gmail.com>
2013-01-06 15:50:11 +0000

    Set up basic styles

--- site/templates/default.php
+++ site/templates/default.php
@@ -5,10 +5,10 @@
 <section class="content">
 
   <article>
-    <h1><?php echo html($page->title()) ?></h1>
-    <?php echo kirbytext($page->description()) ?>
+    <h2><?php echo html($page->title()); ?></h2>
+    <?php echo kirbytext($page->description()); ?>
     <?php 
-      
+
       if( has_pattern_file('php.php', $page) ) { echo get_pattern('php.php', $page); }
       if( has_pattern_file('html.html', $page) ) { echo get_pattern('html.html', $page); }
       if( has_pattern_file('css.css', $page) ) { echo get_pattern('css.css', $page); }

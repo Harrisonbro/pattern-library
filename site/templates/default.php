42a0d3b3e8ed6c94e232b4b9c195b48769f6aaca
Harrison Brown (iMac) <harrisonbro@gmail.com>
2013-01-07 12:22:48 +0000

    Added quick links to jump to code blocks

--- site/templates/default.php
+++ site/templates/default.php
@@ -6,6 +6,13 @@
 
   <h2><?php echo html($page->title()); ?></h2>
   <?php echo kirbytext($page->description()); ?>
+  <p>Jump to:
+    <ul class="jump">
+      <? if( has_pattern_file('php.php', $page) ) { echo "<li><a href='#php'>PHP</a></li>"; } ?>
+      <? if( has_pattern_file('html.html', $page) ) { echo "<li><a href='#html'>HTML</a></li>"; } ?>
+      <? if( has_pattern_file('css.css', $page) ) { echo "<li><a href='#css'>CSS</a></li>"; } ?>
+    </ul><!-- .jump -->
+  </p>
   
   <?php if( has_pattern_file('php.php', $page) ) : ?>
     <pre id="php" class="pattern-code"><?php echo get_pattern('php.php', $page); ?></pre><!-- #php -->

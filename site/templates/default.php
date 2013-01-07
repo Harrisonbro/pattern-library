a08b44f6f64775d3ebeb8c06fe4530c13ef716b8
Harrison Brown (iMac) <harrisonbro@gmail.com>
2013-01-07 12:18:18 +0000

    Removed line breaks within <pre> in template that was causing indentation in output

--- site/templates/default.php
+++ site/templates/default.php
@@ -8,21 +8,15 @@
   <?php echo kirbytext($page->description()); ?>
   
   <?php if( has_pattern_file('php.php', $page) ) : ?>
-  <pre id="php" class="pattern-code">
-    <?php echo get_pattern('php.php', $page); ?>
-  </pre><!-- #php -->
+    <pre id="php" class="pattern-code"><?php echo get_pattern('php.php', $page); ?></pre><!-- #php -->
   <?php endif; ?>
   
   <?php if( has_pattern_file('html.html', $page) ) : ?>
-  <pre id="html" class="pattern-code">
-    <?php echo get_pattern('html.html', $page); ?>
-  </pre><!-- #html -->
+    <pre id="html" class="pattern-code"><?php echo get_pattern('html.html', $page); ?></pre><!-- #html -->
   <?php endif; ?>
   
   <?php if( has_pattern_file('css.css', $page) ) : ?>
-  <pre id="css" class="pattern-code">
-    <?php echo get_pattern('css.css', $page); ?>
-  </pre><!-- #css -->
+    <pre id="css" class="pattern-code"><?php echo get_pattern('css.css', $page); ?></pre><!-- #css -->
   <?php endif; ?>
 
 </section>

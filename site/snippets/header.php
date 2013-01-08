5ad911563bfe3c9a04bc70ddb315f713b300b8bf
Harrison Brown (iMac) <harrisonbro@gmail.com>
2013-01-08 11:22:04 +0000

    Added viewport meta tag to header

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,6 +4,8 @@
   
   <title><?php echo html($site->title()) ?> - <?php echo html($page->title()) ?></title>
   <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width">
+  
   <meta name="description" content="<?php echo html($site->description()) ?>" />
   <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
   <meta name="robots" content="index, follow" />

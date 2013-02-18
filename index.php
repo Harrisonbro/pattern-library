625c7850dbe3ac489eac39b325043b522170874b
Harrison Brown (MacBook Pro) <harrisonbro@gmail.com>
2013-02-18 20:12:01 +0000

    Started on system file

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+$root = dirname(__FILE__); 
+$root_site = $root . '/site'; 
+$root_patterns: = $root . '/patterns'; 
+
+// Try to load system
+if( !file_exists($root_site . '/system.php') ) {
+  die( 'Could not load system.php' ); 
+}
+
+require_once( $root_site . '/system.php' ); 
